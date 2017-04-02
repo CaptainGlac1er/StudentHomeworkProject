f3339c9d9f37b70dc7ff14de9188525384d88d86
Dylan Vuz <vuzdylan@gmail.com>
2017-04-02 18:35:35 -0400

    added loign page

--- app/js/pages/index.jsx
+++ app/js/pages/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from 'pages/Home';
+import Login from 'pages/Login';
 import store from 'store';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
@@ -11,6 +12,7 @@ export default () => (
       <BrowserRouter>
         <Switch>
           <Route component={Home} exact path="/" />
+          <Route component={Login} path="/login" />
           <Route component={Home} />
         </Switch>
       </BrowserRouter>
