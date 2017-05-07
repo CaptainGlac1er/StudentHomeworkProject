280ec4021d7a22fd4323691a9bce9e8dcc2a75df
George Colgrove <georgecolgrove@outlook.com>
2017-05-07 15:50:39 -0400

    start

--- app/js/components/Header.jsx
+++ app/js/components/Header.jsx
@@ -29,6 +29,7 @@ class Header extends Component {
       <AppBar
         iconElementLeft={<img src={'./img/icon.png'} style={{ height: '48px', width: '48px' }} alt={'Icon'} />}
         title={this.state.title}
+        iconElementRight={<i />}
       />
     );
   }
