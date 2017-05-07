9be62e3208aa5978665795c557aea9a97199dd08
George Colgrove <georgecolgrove@outlook.com>
2017-05-07 16:21:33 -0400

    fixes

--- app/js/containers/Header.js
+++ app/js/containers/Header.js
@@ -3,7 +3,6 @@ import Header from 'components/Header';
 import { logoutUser } from 'actions/login';
 
 
-
 function mapDispatchToProps(dispatch) {
   return {
     logout: () => dispatch(logoutUser()),
